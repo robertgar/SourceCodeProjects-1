f21e1e1b4b8b0d4370d07d2e969ccca1c4d5b3df
Josh <tec.desarrollo15.gtd@gmail.com>
2022-12-02 14:57:05 -0600

    [Delete] New-old project

--- Generate mail exemption/New-old Code/connection/ConnectToSQL.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.SqlClient;
-
-namespace connection{
-    internal class ConnectToSQL{
-        public SqlConnection getStringConnection() {
-            return new SqlConnection("User ID=JoshuaS;Database=GuatemalaDigital;PASSWORD=malacuac;server=remoto.guatemaladigital.org;Connect Timeout=30");
-        }
-    }
-}
